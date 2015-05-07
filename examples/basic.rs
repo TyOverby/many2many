e69e32e47e1dca89652748a54e7c654cf7e7b100
Ty Overby <ty@pre-alpha.com>
2015-05-06 22:11:34 -0700

    implemented clone_filter

--- examples/basic.rs
+++ examples/basic.rs
@@ -5,11 +5,14 @@ fn main() {
     let (sx, rx)= mchannel();
 
     let r1 = rx.clone();
-    let r2 = rx.clone();
-    let r3 = rx.clone();
+    let r2 = r1.clone_filter(|&x| x % 2 == 0);
+    let r3 = r2.clone_filter(|&x| x < 6);
+
 
     sx.send(5).unwrap();
     sx.send(10).unwrap();
+    sx.send(2).unwrap();
+    sx.send(0).unwrap();
 
     println!("{} {} {}",
              r1.recv().unwrap(),
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use std::thread::spawn;
 use std::sync::mpsc::{channel, Sender, Receiver, RecvError, TryRecvError, Iter};
 use std::convert::From;
+use std::sync::Arc;
 
-type FilterFn<T> = Option<Box<Fn(&T) -> bool + 'static + Send>>;
+type FilterFn<T> = Option<Arc<Box<Fn(&T) -> bool + 'static + Sync + Send>>>;
 
 pub struct MReceiver<T> {
     subscribe: Sender<(Sender<T>, FilterFn<T>)>,
@@ -13,7 +14,7 @@ pub struct MReceiver<T> {
 pub fn mchannel<T: Send + Clone + 'static>() -> (Sender<T>, MReceiver<T>) {
     let (ds, dr) = channel();
 
-    (ds, MReceiver::from_receiver(dr))
+    (ds, MReceiver::from_receiver(dr, None))
 }
 
 fn listen<T: Send + Clone + 'static>(r: Receiver<T>) -> Sender<(Sender<T>, FilterFn<T>)> {
@@ -56,22 +57,23 @@ fn listen<T: Send + Clone + 'static>(r: Receiver<T>) -> Sender<(Sender<T>, Filte
 
 impl <T> From<Receiver<T>> for MReceiver<T> where T: Send + Clone + 'static {
     fn from(other: Receiver<T>) -> MReceiver<T> {
-        MReceiver::from_sub(listen(other))
+        MReceiver::from_sub(listen(other), None)
     }
 }
 
 impl <T> MReceiver<T> where T: Send + Clone + 'static {
-    fn from_receiver(other: Receiver<T>) -> MReceiver<T> {
-        MReceiver::from_sub(listen(other))
+    fn from_receiver(other: Receiver<T>, filter: FilterFn<T>) -> MReceiver<T> {
+        MReceiver::from_sub(listen(other), filter)
     }
 
-    fn from_sub(subscriber: Sender<(Sender<T>, FilterFn<T>)>) -> MReceiver<T> {
+    fn from_sub(subscriber: Sender<(Sender<T>, FilterFn<T>)>,
+                filter: FilterFn<T>) -> MReceiver<T> {
         let (sx, rx)= channel();
-        let _ = subscriber.send((sx, None));
+        let _ = subscriber.send((sx, filter.clone()));
         MReceiver {
             subscribe: subscriber,
             rec: rx,
-            filter: None
+            filter: filter
         }
     }
 
@@ -94,10 +96,31 @@ impl <T> MReceiver<T> where T: Send + Clone + 'static {
     pub fn iter<'a>(&'a self) -> Iter<'a, T> {
         self.rec.iter()
     }
+
+    pub fn clone_filter<F>(&self, f: F) -> MReceiver<T>
+    where F: Fn(&T) -> bool + 'static + Send + Sync
+    {
+        if let Some(cur_fn) = self.filter.as_ref() {
+            let old_f = cur_fn.clone();
+            let new_f = move |a: &T| {
+                old_f(a) && f(a)
+            };
+
+            MReceiver::from_sub(
+                self.subscribe.clone(),
+                Some(Arc::new(
+                     Box::new(new_f) as Box<Fn(&T) -> bool + 'static + Send + Sync>)))
+        } else {
+            MReceiver::from_sub(
+                self.subscribe.clone(),
+                Some(Arc::new(
+                     Box::new(f) as Box<Fn(&T) -> bool + 'static + Send + Sync>)))
+        }
+    }
 }
 
 impl <T> Clone for MReceiver<T> where T: Send + Clone + 'static {
     fn clone(&self) -> MReceiver<T> {
-        MReceiver::from_sub(self.subscribe.clone())
+        MReceiver::from_sub(self.subscribe.clone(), None)
     }
 }
